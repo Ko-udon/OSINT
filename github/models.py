81c0269ba3442939ba0edbeecc016012360fdd63
KoDongWoo <ehddn2202@gmail.com>
2025-01-08 09:28:12 +0900

    🗃️ model 정의

--- github/models.py
+++ github/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class SendUser(models.Model):
+    email = models.EmailField(unique=True)
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
